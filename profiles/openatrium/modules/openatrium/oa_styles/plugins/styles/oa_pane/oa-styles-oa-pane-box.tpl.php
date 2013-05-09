e9bce4c1b1f870006d424ced7ed626d474160106
mpotter <mpotter@phase2technology.com>
2013-05-09 19:56:29 +0000

    Remove previous profile

--- profiles/openatrium/modules/openatrium/oa_styles/plugins/styles/oa_pane/oa-styles-oa-pane-box.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="oa-pane <?php if (isset($settings['extra_class'])) { print $settings['extra_class']; } ?>">
-  <?php print $content; ?>
-</div>
--- profiles/openatrium/modules/openatrium/oa_styles/plugins/styles/well/oa-styles-well-box.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="well style-region <?php if (isset($settings['well_modifier'])) { print $settings['well_modifier']; } ?>">
-  <?php print $content; ?>
-</div>
